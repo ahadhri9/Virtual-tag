2d83fa5c2033e43b719b7a2171aae0cf1623c95f
ahadhri9 <achrafhadhri9@gmail.com>
2022-05-21 15:08:56 +0200

    btns gallery

--- Android/app/src/main/java/com/google/ar/core/examples/java/activities/SignUpActivity.java
+++ Android/app/src/main/java/com/google/ar/core/examples/java/activities/SignUpActivity.java
@@ -91,6 +91,7 @@ public class SignUpActivity extends AppCompatActivity {
         int previewHeight = bitmap.getHeight() * previewWidth / bitmap.getWidth();
         Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth,previewHeight,false);
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG,50,byteArrayOutputStream);
         byte[] bytes = byteArrayOutputStream.toByteArray();
         return Base64.encodeToString(bytes, Base64.DEFAULT);
     }
