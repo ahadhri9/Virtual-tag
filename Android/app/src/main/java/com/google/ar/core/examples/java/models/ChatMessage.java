f8b3db4020cd2fa26fff2ff31b6d885b69e736d9
xx_my <lopezchefchoufi.celian@gmail.com>
2022-05-23 14:12:36 +0200

    push tout

--- Android/app/src/main/java/com/google/ar/core/examples/java/models/ChatMessage.java
+++ Android/app/src/main/java/com/google/ar/core/examples/java/models/ChatMessage.java
@@ -1,5 +1,9 @@
 package com.google.ar.core.examples.java.models;
 
+import java.util.Date;
+
 public class ChatMessage {
     public String senderId , receiverId, message, dateTime;
+    public Date dateObject;
+
 }
